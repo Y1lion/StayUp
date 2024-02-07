af49d44462c2b23fed6c3f2dcd9b443d2424e278
C4MRS <difendiamolanoia@gmail.com>
2024-02-08 00:53:53 +0100

    servlet for changing pt description

---
+++ src/main/java/controller/ServletChangeDescription.java
@@ -0,0 +1,51 @@
+package controller;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import model.personalTrainer.PersonalTrainer;
+import model.personalTrainer.PersonalTrainerDAO;
+import model.user.UserBean;
+import model.user.UserBeanDAO;
+import model.utils.PasswordEncryptionUtil;
+
+import java.io.IOException;
+
+import static java.lang.System.out;
+
+@WebServlet(name = "ChangeDescription", value = "/ChangeDescription")
+public class ServletChangeDescription extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try{
+            HttpSession session = request.getSession();
+            String desc = request.getParameter("newdescription");
+            String psw = request.getParameter("current_password4");
+            psw = PasswordEncryptionUtil.encryptPassword(psw);
+            UserBean ub = new UserBeanDAO().loginUser((String) session.getAttribute("email"),psw);
+
+            if(ub.getEmail().equalsIgnoreCase("ERRORE")){
+                throw new Exception("Wrong password");
+            }
+
+            PersonalTrainer pt = new PersonalTrainerDAO().retrieveInfo((String) session.getAttribute("email"));
+
+            if (pt.getDescription().equalsIgnoreCase(desc)){
+                throw new Exception("New description is the same as the old description");
+            }
+
+            pt = new PersonalTrainerDAO().changeDescription(pt,desc);
+            request.setAttribute("success","./userpage.jsp");
+            request.getRequestDispatcher("./infopages/success.jsp").forward(request, response);
+        }catch (Exception e){
+            request.setAttribute("exception",e);
+            request.setAttribute("exceptionURL","./userpage.jsp");
+            request.getRequestDispatcher("./infopages/error.jsp").forward(request, response);
+        }
+    }
+}
