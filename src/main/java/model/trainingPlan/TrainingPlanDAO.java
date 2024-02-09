296db16118cb9f8c4013b83f38655990eed81a64
C4MRS <difendiamolanoia@gmail.com>
2024-02-09 22:01:13 +0100

    add method to get Training Plans written by the same pt

--- src/main/java/model/trainingPlan/TrainingPlanDAO.java
+++ src/main/java/model/trainingPlan/TrainingPlanDAO.java
@@ -14,6 +14,8 @@ public class TrainingPlanDAO {
         PreparedStatement ps = null;
         /*JSON EXERCISES:
         * TITOLO SCHEDA
+        * GIORNI: ?
+        *
         * SPALLE
         * BICIPITI
         * TRICIPITI
@@ -72,6 +74,39 @@ public class TrainingPlanDAO {
             ps = conn.prepareStatement("SELECT * FROM trainingPlan WHERE emailUser = ? AND dateEnd > ?");
             ps.setString(1, ub.getEmail());
             ps.setDate(2, new Date(System.currentTimeMillis()));
+
+            ResultSet res = ps.executeQuery();
+
+            while(res.next())
+            {
+                trainingPlans.add(new TrainingPlan(res.getString("emailUser"),res.getString("emailPT"), res.getString("exercises"), res.getDate("dateStart"), res.getDate("dateEnd")));
+            }
+            return  trainingPlans;
+
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }finally{
+            try {
+                ps.close();
+                ConnectionPool.releaseConnection(conn);
+            } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+    public synchronized ArrayList<TrainingPlan> getAvailablePtTrainingPLan(UserBean ub, PersonalTrainer pt){
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ArrayList<TrainingPlan> trainingPlans = new ArrayList<>();
+        try {
+            conn = ConnectionPool.getConnection();
+            ps = conn.prepareStatement("SELECT * FROM trainingPlan WHERE emailUser = ? AND emailPT = ? AND dateEnd > ?");
+            ps.setString(1, ub.getEmail());
+            ps.setString(2, pt.getUser().getEmail());
+            ps.setDate(3, new Date(System.currentTimeMillis()));
 
             ResultSet res = ps.executeQuery();
 
