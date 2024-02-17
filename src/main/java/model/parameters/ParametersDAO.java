b59df21ab05603b49c6da849c3e5b9b342e1f3e5
C4MRS <difendiamolanoia@gmail.com>
2024-02-17 01:11:22 +0100

    add method to check parameters pattern

--- src/main/java/model/parameters/ParametersDAO.java
+++ src/main/java/model/parameters/ParametersDAO.java
@@ -67,6 +67,18 @@ public class ParametersDAO {
     public synchronized Parameters setParameters(String email, ArrayList<Double> params){
         Connection connection = null;
         PreparedStatement preparedStatement = null;
+        Parameters param = new Parameters(email);
+        param.setWeight(params.get(0));
+        param.setLean_mass(params.get(1));
+        param.setFat_mass(params.get(2));
+        param.setArm_mis(params.get(3));
+        param.setLeg_mis(params.get(4));
+        param.setChest_mis(params.get(5));
+        param.setHips_mis(params.get(6));
+        param.setShoulders_mis(params.get(7));
+        param.setWorkoutYears(params.get(8).intValue());
+        if (!checkParams(param))
+            return null;
         try{
             connection = ConnectionPool.getConnection();
             preparedStatement = connection.prepareStatement("INSERT INTO parameters values (?,?,?,?,?,?,?,?,?,?)");
@@ -83,16 +95,6 @@ public class ParametersDAO {
             int upd = preparedStatement.executeUpdate();
 
             if(upd != 0) {
-                Parameters param = new Parameters(email);
-                param.setWeight(params.get(0));
-                param.setLean_mass(params.get(1));
-                param.setFat_mass(params.get(2));
-                param.setArm_mis(params.get(3));
-                param.setLeg_mis(params.get(4));
-                param.setChest_mis(params.get(5));
-                param.setHips_mis(params.get(6));
-                param.setShoulders_mis(params.get(7));
-                param.setWorkoutYears(params.get(8).intValue());
                 System.out.print("Parameters registered");
                 preparedStatement.close();
                 ConnectionPool.releaseConnection(connection);
@@ -112,7 +114,27 @@ public class ParametersDAO {
         }
         return null;
     }
-
+    private synchronized Boolean checkParams(Parameters params){
+        if (params.getWeight()<10 || params.getWeight()>400)
+            return false;
+        if (params.getLean_mass()<10 || params.getLean_mass()>400)
+            return false;
+        if (params.getFat_mass()<10 || params.getFat_mass()>400)
+            return false;
+        if (params.getArm_mis()<10 || params.getArm_mis()>100)
+            return false;
+        if (params.getLeg_mis()<10 || params.getLeg_mis()>100)
+            return false;
+        if (params.getChest_mis()<10 || params.getChest_mis()>100)
+            return false;
+        if (params.getHips_mis()<10 || params.getHips_mis()>100)
+            return false;
+        if (params.getShoulders_mis()<10 || params.getShoulders_mis()>100)
+            return false;
+        if (params.getWorkoutYears()<0 || params.getWorkoutYears()>60)
+            return false;
+        return true;
+    }
     /**
      * Change parameters parameters.
      *
@@ -122,6 +144,8 @@ public class ParametersDAO {
     public synchronized Parameters changeParameters(Parameters params){
         Connection connection = null;
         PreparedStatement preparedStatement = null;
+        if (!checkParams(params))
+            return null;
         try {
             connection = ConnectionPool.getConnection();
             preparedStatement = connection.prepareStatement("UPDATE parameters SET weight = ?, lean_mass = ?, fat_mass = ?, arm_mis = ?, leg_mis = ?, chest_mis = ?, hips_mis = ?, shoulders_mis = ?, workoutYears = ? WHERE email = ?");
@@ -148,7 +172,8 @@ public class ParametersDAO {
             e.printStackTrace();
         } finally {
             try {
-                preparedStatement.close();
+                if (preparedStatement!=null)
+                    preparedStatement.close();
                 ConnectionPool.releaseConnection(connection);
             } catch (SQLException e) {
                 // TODO Auto-generated catch block
