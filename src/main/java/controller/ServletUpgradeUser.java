cbc6fafdd7667f9204954c361aa2c7a2ce490770
Andrea <andymatty36@gmail.com>
2024-02-18 23:36:48 +0100

    Fixed email length validation for ServletSendEmail

--- src/main/java/controller/ServletUpgradeUser.java
+++ src/main/java/controller/ServletUpgradeUser.java
@@ -21,17 +21,26 @@ public class ServletUpgradeUser extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String email=request.getParameter("email");
-        UserBeanDAO ubd=new UserBeanDAO();
-        UserBean ub=ubd.recoverInfos(email);
-        ubd.upgradeToPT(ub);
-        JSONObject jsonUser = new JSONObject();
-        jsonUser.put("email", ub.getEmail());
-        jsonUser.put("nome", ub.getNome());
-        jsonUser.put("cognome", ub.getCognome());
-        jsonUser.put("telefono", ub.getTelefono());
-        // Invia la risposta contenente l'oggetto User in formato JSON
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
-        response.getWriter().write(jsonUser.toString());
+        try {
+            if (!email.matches("^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,})+$"))
+                throw new Exception("Email format is not respected");
+            if (email.length() < 6 || email.length() > 40)
+                throw new Exception("Email lenght is not respected");
+            UserBeanDAO ubd = new UserBeanDAO();
+            UserBean ub = ubd.recoverInfos(email);
+            ubd.upgradeToPT(ub);
+            JSONObject jsonUser = new JSONObject();
+            jsonUser.put("email", ub.getEmail());
+            jsonUser.put("nome", ub.getNome());
+            jsonUser.put("cognome", ub.getCognome());
+            jsonUser.put("telefono", ub.getTelefono());
+            // Invia la risposta contenente l'oggetto User in formato JSON
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+        }catch (Exception e){
+            request.setAttribute("exception",e);
+            request.setAttribute("exceptionURL","./userpage.jsp");
+            request.getRequestDispatcher("./infopages/error.jsp").forward(request,response);
+        }
     }
 }
