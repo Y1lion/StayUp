4f9af1c1bd0c123912365dc4a01351085200e8e9
Andrea <andymatty36@gmail.com>
2024-02-22 19:07:40 +0100

    fixed ServletSendEmail bug

---
+++ src/main/java/controller/ServletSendEmail.java
@@ -0,0 +1,53 @@
+package controller;
+
+import com.postmarkapp.postmark.Postmark;
+import com.postmarkapp.postmark.client.ApiClient;
+import com.postmarkapp.postmark.client.data.model.message.Message;
+import com.postmarkapp.postmark.client.data.model.message.MessageResponse;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import model.user.UserBean;
+import model.user.UserBeanFacade;
+import model.utils.PasswordEncryptionUtil;
+import model.utils.RandomStringGenerator;
+
+
+import java.io.IOException;
+
+
+@WebServlet("/sendEmail")
+public class ServletSendEmail extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            String emailuser=request.getParameter("emailLog");
+            if (!emailuser.matches("^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,})+$"))
+                throw new Exception("Email format is not respected");
+            if(emailuser.length()<6 || emailuser.length()>40)
+                throw new Exception("Email lenght is not respected");
+            String psw=RandomStringGenerator.generateRandomString();
+            UserBean ub=new UserBeanFacade().checkEmail(emailuser);
+            if (ub.getEmail().equalsIgnoreCase("ERRORE"))
+                throw new Exception("Account doesn't exist");
+            ApiClient client = Postmark.getApiClient(System.getenv("API_KEY"));
+            Message message = new Message("a.abbate20@studenti.unisa.it", emailuser, "New Password for StayUp", "Your password: "+psw);
+            message.setMessageStream("stayup");
+            MessageResponse sending = client.deliverMessage(message);
+            psw=PasswordEncryptionUtil.encryptPassword(psw);
+            new UserBeanFacade().forgotPsw(ub,psw);
+            System.out.println("Email sent successfully!");
+            request.setAttribute("success","./login.jsp");
+            request.getRequestDispatcher("./infopages/success.jsp").forward(request,response);
+        } catch (Exception e) {
+            request.setAttribute("exception",e);
+            request.setAttribute("exceptionURL","./userpage.jsp");
+            request.getRequestDispatcher("./infopages/error.jsp").forward(request,response);
+        }
+    }
+}
